684fb2d84e5b6f543685a3029469b5351680ebae
suraj kumar <surajshk1995@gmail.com>
2024-05-27 12:42:48 +0530

    start js course topice wise

---
+++ 02-datatype.js
@@ -0,0 +1,21 @@
+//data type are two type 
+//1 premitive dattype  2  non premitive datatye
+
+//1 premitive datatype
+// number => 2 power 53 range
+// bigint
+// string
+// boolean
+// null
+// undefind => not define value
+// symbool
+/* multiple line comment in jss */
+
+/*non- premitive datatype
+array, object, function*/
+
+let name = "hitesh"
+let age = 22
+let isLogin = true;
+
+console.log(typeof isLogin)
