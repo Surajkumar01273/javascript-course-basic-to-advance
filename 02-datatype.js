b047c35d482a0de9924478a5b29da7424e549640
suraj kumar <surajshk1995@gmail.com>
2024-05-28 10:54:17 +0530

    add datatypes

--- 02-datatype.js
+++ 02-datatype.js
@@ -17,5 +17,47 @@ array, object, function*/
 let name = "hitesh"
 let age = 22
 let isLogin = true;
+let accountCity 
+const temprter = null;
+const bigNumber = 5454246877n;
+const id = Symbol(2222);
+const id1 = 2223;
 
-console.log(typeof isLogin)
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isLogin)
+// console.log(typeof accountCity);
+// console.log(temprter);
+// console.log(typeof temprter);
+// console.log(bigNumber);
+// console.log(typeof bigNumber);
+// console.log(typeof id);
+// console.log(typeof id1);
+
+//  Array  ****************
+
+// const myArray = [1, 3, 56, 34];
+// console.log(myArray);
+// console.log(typeof myArray);
+//  // Object  *************
+
+//  const myobject = {
+//     accounName: "Suraj kumar",
+//     accountId: "121221",
+//     accountPassword: "11111"
+     
+//  }
+//  console.log(myobject);
+//  console.log(typeof myobject);
+
+ // function   ***************
+
+ function app(){
+    const num1 = 4;
+    const num2 = 2;
+    const sum = num1+num2;
+    console.log(sum);
+    return sum
+ }
+app()
